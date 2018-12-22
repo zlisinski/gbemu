376cb9914d58b86b8a621b06e6423cbf4b6803e6
Zach Lisinski <zach@zachlisinski.com>
2018-12-22 14:48:11 -0500

    Fixed tests by adding SDL and TTF Init code.

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,10 +1,29 @@
 #include <gtest/gtest.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include "EmulatorTest.h"
 
 bool debugOutput = false;
 
 int main(int argc, char **argv)
 {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) != 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    if (TTF_Init() != 0)
+    {
+        printf("TTF_Init Error: %s\n", TTF_GetError());
+        exit(1);
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    int ret = RUN_ALL_TESTS();
+
+    TTF_Quit();
+    SDL_Quit();
+
+    return ret;
 }
