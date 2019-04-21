4ed65f4b16688478ef97e773dad17b555f82a979
Zach Lisinski <zach@zachlisinski.com>
2019-04-20 23:59:23 -0400

    Fixed logger macros by removing semicolons.

--- src/Logger.h
+++ src/Logger.h
@@ -70,8 +70,8 @@ private:
     static LogLevel logLevel;
 };
 
-#define LogError(...) do {Logger::Log(LogLevel::eError, __VA_ARGS__);} while (0);
-#define LogWarning(...) do {Logger::Log(LogLevel::eWarning, __VA_ARGS__);} while (0);
-#define LogInfo(...) do {Logger::Log(LogLevel::eInfo, __VA_ARGS__);} while (0);
-#define LogDebug(...) do {Logger::Log(LogLevel::eDebug, __VA_ARGS__);} while (0);
-#define LogInstruction(...) do {Logger::Log(LogLevel::eInstruction, __VA_ARGS__);} while (0);
+#define LogError(...) do {Logger::Log(LogLevel::eError, __VA_ARGS__);} while (0)
+#define LogWarning(...) do {Logger::Log(LogLevel::eWarning, __VA_ARGS__);} while (0)
+#define LogInfo(...) do {Logger::Log(LogLevel::eInfo, __VA_ARGS__);} while (0)
+#define LogDebug(...) do {Logger::Log(LogLevel::eDebug, __VA_ARGS__);} while (0)
+#define LogInstruction(...) do {Logger::Log(LogLevel::eInstruction, __VA_ARGS__);} while (0)
